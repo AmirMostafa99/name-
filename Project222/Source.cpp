8369b209f36501f4d76269e9c30cdaca42ec3b71
AmirMostafa99 <am7870878@gmail.com>
2021-11-01 08:24:49 +0200

    add the files

---
+++ Project222/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string> 
+using namespace std;
+int main() {
+	string n;
+	cout << " Entr your name ....." << endl;
+	cin >> n;
+	cout << " welcome ya abn elars " << n << endl;
+	system("puose");
+	
+}
